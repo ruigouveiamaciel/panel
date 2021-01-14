4b5ff97c6e62d22b57a5fdabf8969a5e8d074b14
Rui Maciel <ruigouveiamaciel@gmail.com>
2021-01-14 14:53:14 +0000

    fixed login screen

--- resources/scripts/components/auth/LoginFormContainer.tsx
+++ resources/scripts/components/auth/LoginFormContainer.tsx
@@ -37,9 +37,9 @@ export default forwardRef<HTMLFormElement, Props>(({ title, ...props }, ref) => 
         }
         <FlashMessageRender css={tw`mb-2 px-1`}/>
         <Form {...props} ref={ref}>
-            <div css={tw`md:flex w-full bg-white shadow-lg rounded-lg p-6 md:pl-0 mx-1`}>
+            <div css={tw`w-full bg-white shadow-lg rounded-lg p-6 mx-1`}>
                 <div css={tw`flex-none select-none mb-6 md:mb-0 self-center`}>
-                    <img src={'/assets/svgs/pterodactyl.svg'} css={tw`block w-48 md:w-64 mx-auto`}/>
+                    <img src={'/logo.png'} css={tw`block w-48 md:w-64 mx-auto`}/>
                 </div>
                 <div css={tw`flex-1`}>
                     {props.children}
@@ -47,14 +47,13 @@ export default forwardRef<HTMLFormElement, Props>(({ title, ...props }, ref) => 
             </div>
         </Form>
         <p css={tw`text-center text-neutral-500 text-xs mt-4`}>
-            &copy; 2015 - 2020&nbsp;
             <a
                 rel={'noopener nofollow noreferrer'}
-                href={'https://pterodactyl.io'}
+                href={'https://ninegaming.pt'}
                 target={'_blank'}
                 css={tw`no-underline text-neutral-500 hover:text-neutral-300`}
             >
-                Pterodactyl Software
+                Nine Gaming
             </a>
         </p>
     </Container>
